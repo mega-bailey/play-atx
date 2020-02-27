4220a4386c37b9a3e0d3d6654b8ae94796719681
Megan Bailey <megan-bailey@outlook.com>
2020-02-26 16:13:49 -0800

    PHP for heroku

--- app.js
+++ app.js
@@ -2,17 +2,12 @@ function parallax(element, distance, speed) {
   const item = document.querySelector(element);
   item.style.transform = `translateY(${distance * speed}px)`;
 }
-// function parallaxX(element, distance, speed) {
-//   const item = document.querySelector(element);
-//   item.style.transform = `translateX(${distance * speed}px)`;
-// }
 
 window.addEventListener('scroll', function() {
   parallax('header', window.scrollY, 0.3);
   parallax('.circle__yellow-pink', window.scrollY, 0.5);
   parallax('.circle__teal-large', window.scrollY, 0.75);
   parallax('.circle__blue-med', window.scrollY, 0.3);
-  // parallax('.btn--main', window.scrollY, 0.3);
   parallax('.circle__coral', window.scrollY, 0.73);
   parallax('.circle__blue-small', window.scrollY, 0.63);
 });
